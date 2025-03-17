5e37dd8c2adc530ad0762717ba585b2018938349
Ayush021 <12114021@nitkkr.ac.in>
2025-03-18 03:02:02 +0530

    added dyanmic color of navtext according to route

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -1,8 +1,11 @@
+
 import React from "react";
 import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
 
+
 const Layout = ({ children }) => {
+ 
   return (
     <div className="flex flex-col min-h-screen">
       <div>
