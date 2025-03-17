39fccde9223bbc5784f8a44c85c4ffd0ddf46048
Govind Ji <jigovind556@gmail.com>
2025-03-18 01:32:32 +0530

    completed about us page

--- app/(root)/about-us/page.js
+++ app/(root)/about-us/page.js
@@ -1,16 +1,18 @@
 import React from "react";
 import MissionVision from "@/components/about/MissionVision";
 import CareProcess from "@/components/about/CareProcess";
-import DoctorProfile from "@/components/about/DoctorProfile";
 import ContactBanner from "@/components/common/ContactBanner";
 import LocationMap from "@/components/common/LocationMap";
+import AboutDoctor from "@/components/about/AboutDoctor";
+import AboutStaff from "@/components/about/AboutStaff";
 
 const AboutUs = () => {
   return (
     <>
       <MissionVision />
       <CareProcess />
-      <DoctorProfile />
+      <AboutDoctor />
+      <AboutStaff />
       <ContactBanner />
       <LocationMap />
     </>
